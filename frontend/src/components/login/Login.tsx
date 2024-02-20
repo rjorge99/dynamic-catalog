e9c6200d53859905b344c34f5dff560a2ef550c1
Jorge Reyes <rjorge99@hotmail.com>
2024-02-19 21:45:34 -0600

    Se quita carpeta de login

--- frontend/src/components/login/Login.tsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-};
-
-export default Login;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Login from './components/login/Login';
 function App() {
-    return <Login />;
+  return <Login />;
 }
 
 export default App;
