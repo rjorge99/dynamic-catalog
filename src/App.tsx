0c9b1ba69a888207f804fbbde269e9a61d30b50e
Jorge Reyes <rjorge99@hotmail.com>
2024-02-20 16:17:01 -0600

    Se empieza a usar mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Button, CssBaseline } from '@mui/material';
 import Login from './views/Login';
+import LoginMaterial from './views/LoginMaterial';
+import LoginCustom from './views/LoginCustom';
 
 function App() {
-  return <Login />;
+    return (
+        <>
+            <CssBaseline />
+            {/* <LoginMaterial /> */}
+            {/* <Login /> */}
+            <LoginCustom />
+        </>
+    );
 }
 
 export default App;
