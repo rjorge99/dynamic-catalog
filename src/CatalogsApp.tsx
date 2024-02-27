aa3729fc94a72435065b26db0b5c0fa6686ed49e
Jorge Reyes <rjorge99@hotmail.com>
2024-02-26 22:48:20 -0600

    Se cambia nombre de archivo y de componente

---
+++ src/CatalogsApp.tsx
@@ -0,0 +1,48 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useAuthStore } from './stores/auth-store';
+import { useEffect } from 'react';
+import { useMemo } from 'react';
+import { useUIStore } from './stores/ui-store';
+import { Outlet } from 'react-router-dom';
+import { useCatalogsStore } from './stores/catalogs-store';
+
+function CatalogsApp() {
+   const setLoggedUser = useAuthStore((store) => store.setLoggedUser);
+   const loadCatalogStructures = useCatalogsStore((store) => store.loadCatalogStructures);
+   const colorMode = useUIStore((store) => store.colorMode);
+   const auth = getAuth();
+
+   useEffect(() => {
+      onAuthStateChanged(auth, (user) => {
+         const isUserLogged = !!user;
+         const loggedUser = isUserLogged
+            ? {
+                 uid: user.uid!,
+                 displayName: user.displayName!,
+                 photoURL: user.photoURL!
+              }
+            : null;
+
+         if (isUserLogged) loadCatalogStructures(loggedUser?.uid!);
+         setLoggedUser(loggedUser);
+      });
+   }, []);
+
+   const currentTheme = useMemo(() => {
+      return createTheme({
+         palette: {
+            mode: colorMode
+         }
+      });
+   }, [colorMode]);
+
+   return (
+      <ThemeProvider theme={currentTheme}>
+         <CssBaseline />
+         <Outlet />
+      </ThemeProvider>
+   );
+}
+
+export default CatalogsApp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import CatalogsApp from './CatalogsApp.tsx';
 import './index.css';
+import { Navigate, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import PrivateRoutes from './routes/private-routes.tsx';
+import Layout from './layouts/Layout.tsx';
+import DashBoard from './views/DashBoard.tsx';
+import PublicRoutes from './routes/public-routes.tsx';
+import Login from './views/auth/Login.tsx';
+import Register from './views/auth/Register.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>
+const router = createBrowserRouter(
+   createRoutesFromElements(
+      <Route element={<CatalogsApp />}>
+         <Route
+            path='/*'
+            element={
+               <PrivateRoutes>
+                  <Routes>
+                     <Route element={<Layout />}>
+                        <Route index element={<DashBoard />} />
+                        <Route path='*' element={<Navigate to='/' replace />} />
+                     </Route>
+                  </Routes>
+               </PrivateRoutes>
+            }
+         />
+         <Route
+            path='/auth/*'
+            element={
+               <PublicRoutes>
+                  <Routes>
+                     <Route path='/login' element={<Login />} />
+                     <Route path='/register' element={<Register />} />
+                     <Route path='*' element={<Navigate to='/auth/login' replace />} />
+                  </Routes>
+               </PublicRoutes>
+            }
+         />
+      </Route>
+   ),
+   { basename: '/dynamic-catalog' }
 );
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
